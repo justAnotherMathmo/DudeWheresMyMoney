e27dea79bb81df79f088da57d38b7ecde0624c42
Kaimyn Chapman <kchap21@hotmail.com>
2018-05-24 22:06:31 +0100

    Added navigation past both login screens (!!)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 import requests
+import time
 
 data = {
     'frmLogin:strCustomerLogin_userID': '',
@@ -8,15 +9,15 @@ data = {
 memorable_info = ''
 
 
-def get_token(name, content):
+def get_token(name: str, content: str) -> str:
     return re.search(f'<input\s*type="hidden"\s*name="{name}"\s*value="([0-9a-zA-Z_]*)"\s*/>', content)[1]
 
 
-def pprint_req(req):
+def pprint_req(req: requests.models.Response) -> None:
     print(req.content.decode('utf-8'))
 
 
-def get_mem_info_positions(content):
+def get_mem_info_positions(content: str) -> list:
     positions = []
     for i in [1, 2, 3]:
         # Minus 1 because indexes != labels
@@ -24,7 +25,7 @@ def get_mem_info_positions(content):
     return positions
 
 
-if __name__ == '__main__':
+def get_logged_in_session():
     # Set up session
     sess = requests.Session()
 
@@ -35,18 +36,30 @@ if __name__ == '__main__':
     # Get hidden tokens on page to
     data['submitToken'] = get_token('submitToken', req1_content)
 
-    print(data)
+    # Just wait a little bit, because web servers don't like you hammering them
+    time.sleep(2)
 
     # Log in past page one, and get the "memorable information page"
     req2 = sess.post(r'https://online.lloydsbank.co.uk/personal/primarylogin', data=data)
     req2_content = req2.content.decode('utf-8')
-    data2 = {'submitToken': get_token('submitToken', req2_content)}
+    data2 = {'submitToken': get_token('submitToken', req2_content),
+             'frmentermemorableinformation1': 'frmentermemorableinformation1',
+             'frmentermemorableinformation1:btnContinue': 'null'
+             }
 
     for i, mem_pos in enumerate(get_mem_info_positions(req2_content)):
-        data2[f'frmentermemorableinformation1:strEnterMemorableInformation_memInfo{i+1}'] = '&amp;nbsp;' + memorable_info[mem_pos]
+        data2[f'frmentermemorableinformation1:strEnterMemorableInformation_memInfo{i+1}'] = '&nbsp;' + memorable_info[
+            mem_pos]
 
-    print(data2)
+    # Just wait a little bit, because web servers don't like you hammering them
+    time.sleep(2)
 
     # Log in past memorable information page? (doesn't work yet)
-    req3 = sess.post(r'https://online.lloydsbank.co.uk/personal/a/logon/entermemorableinformation.jsp', data=data2,
-                     headers=dict(referer=r'https://online.lloydsbank.co.uk/personal/a/logon/entermemorableinformation.jsp'))
+    req3 = sess.post(r'https://secure.lloydsbank.co.uk/personal/a/logon/entermemorableinformation.jsp', data=data2)
+
+    return sess, req3
+
+
+if __name__ == '__main__':
+    session, main_page = get_logged_in_session()
+    
